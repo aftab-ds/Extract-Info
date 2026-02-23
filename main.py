8def6b1ed84e0865a9735941d3d53cb9e0aa69df
aftab-ds <aftab776777@gmail.com>
2026-02-23 11:42:54 +0000

    CORS

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, APIRouter
 from routers import fetch_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change this in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(fetch_router.router)
